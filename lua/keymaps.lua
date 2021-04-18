8aa773f4df0520d4787bd9628ccbf42fb803fb8d
TheNerdyHamster <leo@letnh.com>
2021-04-18 22:47:47 +0200

    Disable due to tmux conflict

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,6 +1,7 @@
 local utils = require('utils')
 
-utils.map('n', '<C-l>', '<cmd>noh<CR>') -- Clear highlights
+-- Diasble due to conflict with tmux
+-- utils.map('n', '<C-l>', '<cmd>noh<CR>') -- Clear highlights
 
 -- Esc
 utils.map('i', 'jk', '<Esc>')
@@ -16,7 +17,7 @@ utils.map('i', '<right>', '<nop>')
 
 -- Buffers
 utils.map('n', '<Leader>bb', '<cmd>buffers<CR>:buffer<Space>')
-utils.map('n', '<Leader>bc', '<cmd>BufferClose<CR>')
+utils.map('n', '<Leader>bc', '<cmd>BufferClose<R>')
 utils.map('n', '<Leader>bC', '<cmd>BufferClose!<CR>')
 utils.map('n', '<Leader>bp', '<cmd>BufferPrevious<CR>')
 utils.map('n', '<Leader>bn', '<cmd>BufferNext<CR>')
