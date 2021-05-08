3793f317d53792b7405836f1fb0871aa9c08266d
Leo Rönnebro <leo.ronnebro@nerdyhamster.net>
2021-05-08 23:33:27 +0200

    [Fixes] Completion, Csharp

--- lua/lang/helpers.lua
+++ lua/lang/helpers.lua
@@ -1,12 +1,9 @@
 local _M = {}
 
 _M.on_attach = function(client, bufnr)
-    require('completion').on_attach()
 
     local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
     local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-
-    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
     -- Mappings.
     local opts = { noremap=true, silent=true }
