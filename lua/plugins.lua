0b6ff136825f956a4567d45340c1ef12834039a4
TheNerdyHamster <leo@letnh.com>
2021-05-13 21:29:05 +0200

    [Fixes] Packer installation + deleted unused file

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,7 +3,7 @@ local fn = vim.fn
 -- Auto install packer.nvim if not exists
 local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-  execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
+  os.execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
 end
 
 vim.cmd [[packadd packer.nvim]]
