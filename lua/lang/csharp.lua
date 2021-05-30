bab0ace94d5c66cf51dde446ccefca5355644203
TheNerdyHamster <leo@letnh.com>
2021-05-30 20:28:00 +0200

    [LSP] Langauge client is now fixed

--- lua/lang/csharp.lua
+++ lua/lang/csharp.lua
@@ -5,8 +5,8 @@ local nvim_lsp = require('lspconfig')
 
 local bin = "omnisharp"
 
-nvim_lsp.omnisharp.setup {
+nvim_lsp.omnisharp.setup ({
   capabilities = helper.capabilities;
   on_attach = helper.on_attach;
   cmd = { bin, "-lsp", "--hostPID", tostring(pid) };
-}
+})
