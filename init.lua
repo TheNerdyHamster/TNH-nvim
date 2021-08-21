2a0e8a2dae61998d45f136afb199c77073f3b333
TheNerdyHamster <leo@letnh.com>
2021-08-21 19:55:45 +0200

    Init of pkg manager

--- init.lua
+++ init.lua
@@ -1,3 +1,14 @@
+-- Install packer
+local execute = vim.api.nvim_command
+local fn = vim.fn
+
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+  execute 'packadd packer.nvim'
+end
+ 
 -- Set map leader to space
 vim.g.mapleader = ' '
 
