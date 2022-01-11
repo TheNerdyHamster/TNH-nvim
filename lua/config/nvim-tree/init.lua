7974fd93b6c1724e2bf33a402d87b848045e8e68
Leo Rönnebro <leo@letnh.com>
2022-01-11 09:39:22 +0100

    Fixed nvim-tree

--- lua/config/nvim-tree/init.lua
+++ lua/config/nvim-tree/init.lua
@@ -1,11 +1,15 @@
 local utils = require('utils')
 
+require('nvim-tree').setup {
+  auto_close  = true
+}
+
 vim.g.nvim_tree_side = 'right'
 vim.g.nvim_tree_auto_ignore_ft = { 'dashboard' }
 vim.g.nvim_tree_quit_on_open = 1
 vim.g.nvim_tree_indent_markers = 1
 vim.g.nvim_tree_add_traoling = 1
-vim.g.nvim_tree_lsp_diagnostics = 1
+vim.g.lsp_diagnostics = 1
 vim.g.nvim_tree_special_files = { 'README.md', 'Dockerfile', 'docker-compse.yml' }
 
 utils.map('n', '<Leader>n', '<cmd>NvimTreeToggle<CR>')
