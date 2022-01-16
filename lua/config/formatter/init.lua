285c1efb741c57743300edfe7f6ff60d99274ae2
TheNerdyHamster <leo@letnh.com>
2022-01-16 23:15:38 +0100

    Fixed autoformatting on write

--- lua/config/formatter/init.lua
+++ lua/config/formatter/init.lua
@@ -1,27 +1,31 @@
-local utils = require('utils')
+local utils = require("utils")
 
-require('formatter').setup({
-  filetype = {
-    lua = {
-      function()
-        return {
-          exe = "luafmt",
-          args = { "--indent-count", 2, "--stdin" },
-          stdin = true
-        }
-      end
-    },
+require("formatter").setup(
+  {
+    filetype = {
+      lua = {
+        function()
+          return {
+            exe = "luafmt",
+            args = {"--indent-count", 2, "--stdin"},
+            stdin = true
+          }
+        end
+      }
+    }
   }
-})
+)
 
-vim.api.nvim_exec([[
+vim.api.nvim_exec(
+  [[
 augroup FormatAutogroup
   autocmd!
-  autocmd BufWritePost *.go, *.lua undojoin | FormatWrite
+  autocmd BufWritePost *.lua FormatWrite
 augroup END
-]], true)
+]],
+  true
+)
 
---     autocmd BufWritePre *.go,*.lua,*.sql undojoin | Neoformat
 -- Bindings
-utils.map('n', '<Leader>x', '<cmd>Format<CR>')
+utils.map("n", "<Leader>x", "<cmd>Format<CR>")
 -- }}}
