dd66b8236787b64a72622d6c1130769d338d4e6a
Leo Olofsson <leo@letnh.com>
2022-08-06 15:43:32 +0200

    Updated neogit to latest version

--- lua/config/neogit/init.lua
+++ lua/config/neogit/init.lua
@@ -1,10 +1,9 @@
 local neogit = require('neogit')
 local utils = require('utils')
 
-neogit.setup{
-  disable_context_highlighting = true,
-}
--- utils.map('n', '<Leader>gs', '<cmd>Git<CR>')
--- utils.map('n', '<Leader>gc', '<cmd>Git commit<CR>')
+neogit.setup{}
+
+utils.map('n', '<Leader>gs', '<cmd>Neogit<CR>')
+utils.map('n', '<Leader>gc', '<cmd>Neogit commit<CR>')
 -- utils.map('n', '<Leader>gp', '<cmd>Git push<CR>')
 -- utils.map('n', '<Leader>gP', '<cmd>Git pull<CR>')
