7685aedb77c59d6fe3d981966747eeccd3531853
TheNerdyHamster <leo@letnh.com>
2022-08-06 16:45:47 +0200

    Moved autoformatting for whitespaces to formatter

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,35 +1,26 @@
-local utils = require('utils')
+local utils = require("utils")
 
 local cmd = vim.cmd
 local indent = 2
 
-cmd 'syntax enable'
-cmd 'filetype plugin indent on'
+cmd("syntax enable")
+cmd("filetype plugin indent on")
 
-utils.opt('b', 'expandtab', true)
-utils.opt('b', 'shiftwidth', indent)
-utils.opt('b', 'smartindent', true)
-utils.opt('b', 'tabstop', indent)
-utils.opt('o', 'hidden', true)
-utils.opt('o', 'ignorecase', true)
-utils.opt('o', 'scrolloff', 4)
-utils.opt('o', 'shiftround', true)
-utils.opt('o', 'smartcase', true)
-utils.opt('o', 'splitbelow', true)
-utils.opt('o', 'splitright', true)
-utils.opt('o', 'wildmode', 'list:longest')
-utils.opt('w', 'number', true)
-utils.opt('o', 'relativenumber', true)
-utils.opt('o', 'clipboard', 'unnamed,unnamedplus')
-utils.opt('o', 'completeopt', 'menuone,noselect')
+utils.opt("b", "expandtab", true)
+utils.opt("b", "shiftwidth", indent)
+utils.opt("b", "smartindent", true)
+utils.opt("b", "tabstop", indent)
+utils.opt("o", "hidden", true)
+utils.opt("o", "ignorecase", true)
+utils.opt("o", "scrolloff", 4)
+utils.opt("o", "shiftround", true)
+utils.opt("o", "smartcase", true)
+utils.opt("o", "splitbelow", true)
+utils.opt("o", "splitright", true)
+utils.opt("o", "wildmode", "list:longest")
+utils.opt("w", "number", true)
+utils.opt("o", "relativenumber", true)
+utils.opt("o", "clipboard", "unnamed,unnamedplus")
+utils.opt("o", "completeopt", "menuone,noselect")
 
-cmd 'au TextYankPost * lua vim.highlight.on_yank {on_visual = false}'
-
-vim.api.nvim_exec([[
-    fun! TrimWhitespace()
-        let l:save = winsaveview()
-        keeppatterns %s/\s\+$//e
-        call winrestview(l:save)
-    endfun
-    autocmd FileType go,rust,html,sql autocmd BufWritePre <buffer> call TrimWhitespace()
-]], false)
+cmd("au TextYankPost * lua vim.highlight.on_yank {on_visual = false}")
