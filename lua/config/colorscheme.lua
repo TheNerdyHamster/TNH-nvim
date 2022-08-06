1137de28e7b71a87d15d707c6048a232462e0d29
TheNerdyHamster <leo@letnh.com>
2022-08-06 15:44:40 +0200

    Update colorscheme values

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -4,4 +4,6 @@ utils.opt('o', 'termguicolors', true)
 vim.g.nord_contrast = true
 vim.g.nord_borders = true
 vim.g.nord_disable_background = true
+-- vim.g.nord_enable_sidebar_background = true
+vim.g.nord_uniform_diff_background = true
 require('nord').set()
