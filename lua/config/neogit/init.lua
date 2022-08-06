4680058c9be9a6846792f62e95d468eed9eeb8cd
TheNerdyHamster <leo@letnh.com>
2022-08-06 16:19:59 +0200

    Added autocommands for neogit, edited neogit setup

--- lua/config/neogit/init.lua
+++ lua/config/neogit/init.lua
@@ -1,9 +1,20 @@
 local neogit = require('neogit')
 local utils = require('utils')
+local group = vim.api.nvim_create_augroup('NeogitCustomEvents', { clear = true })
 
-neogit.setup{}
+-- Neogit setup
+neogit.setup{
+  disable_insert_on_commit = false,
+  disable_commit_confirmation = true,
+}
 
+-- Keybindings
 utils.map('n', '<Leader>gs', '<cmd>Neogit<CR>')
 utils.map('n', '<Leader>gc', '<cmd>Neogit commit<CR>')
--- utils.map('n', '<Leader>gp', '<cmd>Git push<CR>')
--- utils.map('n', '<Leader>gP', '<cmd>Git pull<CR>')
+
+-- Custom autocommands
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'NeogitPushComplete',
+  group = group,
+  callback = require('neogit').close,
+})
