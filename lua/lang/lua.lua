75197cc179fd00c088c1fb347275575e8aa8e0b7
Leo Olofsson <leo@letnh.com>
2022-08-07 00:55:12 +0200

    Replaced nlua with native nvim-lsp

--- lua/lang/lua.lua
+++ lua/lang/lua.lua
@@ -1,9 +1,23 @@
-require("nlua.lsp.nvim").setup(require("lspconfig"), {
-	capabilities = capabilities,
-	on_attach = on_attach,
-	init_options = {
-		onlyAnalyzeProjectsWithOpenFiles = true,
-		suggestFromUnimportedLibraries = false,
-		closingLabels = true,
+local helper = require("lang.helpers")
+local nvim_lsp = require("lspconfig")
+
+nvim_lsp.sumneko_lua.setup({
+	capabilities = helper.capabilities,
+	on_attach = helper.on_attach,
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = {
+					"vim",
+					"table",
+					"ipairs",
+					"require",
+					"use",
+				},
+			},
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+		},
 	},
 })
