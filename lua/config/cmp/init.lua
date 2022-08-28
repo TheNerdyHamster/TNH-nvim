682ea9de42599ca071b65dc099117a25b1f8ec43
Leo Olofsson <leo@letnh.com>
2022-08-28 21:03:45 +0200

    Added custom keybindings to cmp

--- lua/config/cmp/init.lua
+++ lua/config/cmp/init.lua
@@ -1,6 +1,15 @@
 -- cmp
 local cmp = require("cmp")
 
+local has_words_before = function()
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local feedkey = function(key, mode)
+	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
+
 cmp.setup({
 	snippet = {
 		expand = function(args)
@@ -16,6 +25,28 @@ cmp.setup({
 		["<C-Space>"] = cmp.mapping.complete(),
 		["<C-e>"] = cmp.mapping.abort(),
 		["<CR>"] = cmp.mapping.confirm({ select = true }),
+		["<C-j>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif vim.fn["vsnip#available"](1) == 1 then
+				feedkey("<Plug>(vsnip-expand-or-jump)", "")
+			elseif has_words_before() then
+				cmp.complete()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+		["<C-k>"] = cmp.mapping(function()
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif vim.fn["vsnip#available"](1) == 1 then
+				feedkey()
+			elseif has_words_before() then
+				cmp.complete()
+			else
+				feedkey("<Plug>(vsnip-jump-prev)", "")
+			end
+		end, { "i", "s" }),
 	}),
 
 	sources = cmp.config.sources({
