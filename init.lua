ee9a6f54f3a370480c055b38bd1581dd020231ad
TheNerdyHamster <leo@letnh.com>
2024-05-13 22:04:36 +0200

    Cleanup for rewrite

--- init.lua
+++
@@ -1,8 +0,0 @@
-vim.loader.enable()
-
-require("settings")
-require("settings.options")
-require("settings.keymap")
-require("plugins")
-
-vim.cmd("colorscheme " .. (dots.UI.colorscheme.name))
--- lua/plugins/specs/comment.lua
+++
@@ -1,4 +0,0 @@
-return {
-  "echasnovski/mini.comment",
-  event = "VeryLazy"
-}
