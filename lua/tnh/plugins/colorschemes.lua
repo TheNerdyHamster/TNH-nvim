041d4b8fe6a7515cd098ba8915bef9229510bce6
TheNerdyHamster <26503572+TheNerdyHamster@users.noreply.github.com>
2025-07-14 13:04:03 +0200

    feat: Added more colorschemes

--- lua/tnh/plugins/colorschemes.lua
+++ lua/tnh/plugins/colorschemes.lua
@@ -3,8 +3,26 @@
 return {
   {
     "folke/tokyonight.nvim",
-    config = function()
-      vim.cmd.colorscheme("tokyonight")
+  },
+  {
+    "eldritch-theme/eldritch.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  {
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function(_, opts)
+      require("cyberdream").setup(opts)
+      vim.cmd.colorscheme("cyberdream")
     end,
+  },
+  {
+    "EdenEast/nightfox.nvim",
+    lazy = false,
+    priority = 1000,
   },
 }
