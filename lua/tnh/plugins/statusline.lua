14c9040a2fea219b01b72dd43af8f2b9affee92e
TheNerdyHamster <26503572+TheNerdyHamster@users.noreply.github.com>
2025-07-14 13:28:58 +0200

    feat: Replaced express_line with lualine

--- lua/tnh/plugins/statusline.lua
+++ lua/tnh/plugins/statusline.lua
@@ -1,11 +1,22 @@
 return {
-  {
-    "tjdevries/express_line.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    config = function()
-      require("tnh.statusline").setup()
-    end,
-  },
+  "nvim-lualine/lualine.nvim",
+  lazy = false,
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("lualine").setup({
+      options = {
+        section_separators = "",
+        component_separators = "",
+        theme = "auto",
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff" },
+        lualine_c = { { "filename", path = 1 } },
+        lualine_x = { "encoding", "fileformat", "filetype" },
+        lualine_y = { "lsp_status", "progress" },
+        lualine_z = { "location" },
+      },
+    })
+  end,
 }
